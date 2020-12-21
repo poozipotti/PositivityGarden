009f662a9cd8a66c9e939a3a6e95209ebedc1049
George Beckert <georgebeckert@gmail.com>
2020-12-21 13:58:06 -0500

    google auth stuff

---
+++ server/export.sh
@@ -0,0 +1,1 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/home/george/Projects/SentimentGarden/server/sentimentGardenSecrets.js"
--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const Koa = require('koa');
 const app = new Koa();
+const language = require('@google-cloud/language');
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+async function quickstart(textToAnalyze) {
+  // Imports the Google Cloud client library
+
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+
+  const document = {
+    content: textToAnalyze,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeSentiment({document: document});
+  return result;
+
+}
+
+app.use( async (ctx) => {
+  const text = "Hello george you look very nice"
+  const result = await quickstart(text);
+
+  ctx.body = {result, text};
 });
 
 app.listen(3000);
